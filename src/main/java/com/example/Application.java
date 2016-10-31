907b89cb508f81ba94b6a1a21169fd12fe9e34c9
sLawalata <sanga.lawalata@gmail.com>
2016-10-31 14:26:40 +0100

    clean up code

--- src/main/java/com/example/Application.java
+++ src/main/java/com/example/Application.java
@@ -1,12 +1,9 @@
 package com.example;
 
 import com.example.utils.StringDistance;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-
-import static org.apache.commons.lang3.StringUtils.getLevenshteinDistance;
 
 @SpringBootApplication
 public class Application {
@@ -19,23 +16,4 @@ public class Application {
     StringDistance stringDistance(){
         return new StringDistance() ;
     }
-
-    @Bean
-    CommandLineRunner cmd() {
-        return cmd -> {
-
-            String noteBookEntry = "Word Words Wor word";
-            String keyword = "Word";
-
-            int levenshteinDistance = getLevenshteinDistance(noteBookEntry, keyword);
-
-            System.out.println("distance : "+getLevenshteinDistance("Word","Word"));
-            System.out.println("distance : "+getLevenshteinDistance("Word","Words"));
-            System.out.println("distance : "+getLevenshteinDistance("Word","Wor"));
-            System.out.println("distance : "+getLevenshteinDistance("Word","word"));
-            System.out.println("distance : "+getLevenshteinDistance("Word","wor"));
-
-        };
-    }
-
 }
